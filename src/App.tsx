a3c4b9dc7c3ef96f5565aadd05576acf06b88a2e
swornimbarahi <sbara002@plattsburgh.edu>
2019-08-13 20:10:13 -0700

    Obeying Prettier standards

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { FunctionComponent } from "react";
 import "./App.scss";
 
 const App: FunctionComponent = () => {
-	return (
-		<div className="App">
-			<div className="ui-m--4">{"Hi! I'm Swornim barahi."}</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="ui-m--4">{"Hi! I'm Swornim barahi."}</div>
+    </div>
+  );
 };
 
 export default App;
