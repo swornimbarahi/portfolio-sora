10005df16f9fb1e9f2e3cf824af20c7f611533a6
swornimbarahi <sbara002@plattsburgh.edu>
2019-08-28 23:06:51 -0400

    fixed security vulnerability from a _blank targets

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -6,29 +6,29 @@ import MediumIcon from "../../assets/medium.png";
 import LinkedInIcon from "../../assets/linkedin.png";
 
 interface IconsPropType {
-	className?: string;
+  className?: string;
 }
 
 export const GitHub: FunctionComponent<IconsPropType> = props => {
-	const { className } = props;
+  const { className } = props;
 
-	return <img src={GithubIcon} alt="" className={className} />;
+  return <img src={GithubIcon} alt="" className={className} />;
 };
 
 export const Gmail: FunctionComponent<IconsPropType> = props => {
-	const { className } = props;
+  const { className } = props;
 
-	return <img src={GmailIcon} alt="" className={className} />;
+  return <img src={GmailIcon} alt="" className={className} />;
 };
 
 export const Medium: FunctionComponent<IconsPropType> = props => {
-	const { className } = props;
+  const { className } = props;
 
-	return <img src={MediumIcon} alt="" className={className} />;
+  return <img src={MediumIcon} alt="" className={className} />;
 };
 
 export const LinkedIn: FunctionComponent<IconsPropType> = props => {
-	const { className } = props;
+  const { className } = props;
 
-	return <img src={LinkedInIcon} alt="" className={className} />;
+  return <img src={LinkedInIcon} alt="" className={className} />;
 };
--- src/sections/MainPage/index.tsx
+++ src/sections/MainPage/index.tsx
@@ -7,64 +7,80 @@ import ProfilePicture from "../../assets/profile_picture.jpg";
 import { Gmail, GitHub, Medium, LinkedIn } from "../../components/Icons";
 
 export const MainPage: FunctionComponent = () => {
-	return (
-		<>
-			<div>
-				<img
-					src={ProfilePicture}
-					alt=""
-					className={classnames(
-						"ui-circle",
-						"ui-position--fixed",
-						"ui-axis--normalize",
-						styles["main-page--profile-placement"],
-						styles["main-page--profile-picture"]
-					)}
-				/>
-			</div>
-			<div>
-				<a href="https://github.com/swornimbarahi" target="_blank">
-					<GitHub
-						className={classnames(
-							"ui-position--fixed",
-							"ui-axis--normalize",
-							styles["main-page--logo"],
-							styles["main-page--logo-placement-github"]
-						)}
-					/>
-				</a>
+  return (
+    <>
+      <div>
+        <img
+          src={ProfilePicture}
+          alt=""
+          className={classnames(
+            "ui-circle",
+            "ui-position--fixed",
+            "ui-axis--normalize",
+            styles["main-page--profile-placement"],
+            styles["main-page--profile-picture"]
+          )}
+        />
+      </div>
+      <div>
+        <a
+          href="https://github.com/swornimbarahi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHub
+            className={classnames(
+              "ui-position--fixed",
+              "ui-axis--normalize",
+              styles["main-page--logo"],
+              styles["main-page--logo-placement-github"]
+            )}
+          />
+        </a>
 
-				<a href="mailto:swornimbarahi@gmail.com" target="_blank">
-					<Gmail
-						className={classnames(
-							"ui-position--fixed",
-							"ui-axis--normalize",
-							styles["main-page--logo"],
-							styles["main-page--logo-placement-gmail"]
-						)}
-					/>
-				</a>
-				<a href="https://linkedin.com/in/swornimbarahi" target="_blank">
-					<LinkedIn
-						className={classnames(
-							"ui-position--fixed",
-							"ui-axis--normalize",
-							styles["main-page--logo"],
-							styles["main-page--logo-placement-linkedin"]
-						)}
-					/>
-				</a>
-				<a href="https://medium.com/@swornimbarahi" target="_blank">
-					<Medium
-						className={classnames(
-							"ui-position--fixed",
-							"ui-axis--normalize",
-							styles["main-page--logo"],
-							styles["main-page--logo-placement-medium"]
-						)}
-					/>
-				</a>
-			</div>
-		</>
-	);
+        <a
+          href="mailto:swornimbarahi@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Gmail
+            className={classnames(
+              "ui-position--fixed",
+              "ui-axis--normalize",
+              styles["main-page--logo"],
+              styles["main-page--logo-placement-gmail"]
+            )}
+          />
+        </a>
+        <a
+          href="https://linkedin.com/in/swornimbarahi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedIn
+            className={classnames(
+              "ui-position--fixed",
+              "ui-axis--normalize",
+              styles["main-page--logo"],
+              styles["main-page--logo-placement-linkedin"]
+            )}
+          />
+        </a>
+        <a
+          href="https://medium.com/@swornimbarahi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Medium
+            className={classnames(
+              "ui-position--fixed",
+              "ui-axis--normalize",
+              styles["main-page--logo"],
+              styles["main-page--logo-placement-medium"]
+            )}
+          />
+        </a>
+      </div>
+    </>
+  );
 };
