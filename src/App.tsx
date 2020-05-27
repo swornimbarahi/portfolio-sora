d401862df86f116f3c9fbdfef8fb8a81c878174b
swornimbarahi <swornimbarahi@gmail.com>
2020-05-27 12:54:12 -0400

    Added sass function to retrieve colors

--- src/App.tsx
+++ src/App.tsx
@@ -15,38 +15,47 @@ import { ExperiencePage } from "./sections/ExperiencePage";
 import { MenuContext } from "./stores/MenuContext";
 import { AboutPage } from "./sections/AboutPage";
 
+import { App as V2 } from "./v2";
+
 const App: FunctionComponent = () => {
-  const [showMenu, setShowMenu] = useState<boolean>();
-  const [showIcons, setShowIcons] = useState<boolean>();
+	const [showMenu, setShowMenu] = useState<boolean>();
+	const [showIcons, setShowIcons] = useState<boolean>();
 
-  useEffect(() => {
-    setShowMenu(false);
-    setShowIcons(true);
-  }, []);
+	useEffect(() => {
+		setShowMenu(false);
+		setShowIcons(true);
+	}, []);
 
-  useEffect(() => {
-    setShowIcons(showMenu || window.location.pathname === "/");
-  }, [showMenu]);
+	useEffect(() => {
+		setShowIcons(showMenu || window.location.pathname === "/");
+	}, [showMenu]);
 
-  return (
-    <div className="ui-position--relative">
-      <Router>
-        <MenuContext.Provider value={{ showMenu, setShowMenu, showIcons }}>
-          <SideMenu />
-          <MainMenu />
-        </MenuContext.Provider>
-        <Background />
-        <Logo />
-        <Switch>
-          <Route path="/" exact component={MainPage} />
-          <Route path="/about" exact component={AboutPage} />
-          <Route path="/projects" exact component={ProjectsPage} />
-          <Route path="/experience" exact component={ExperiencePage} />
-          <Route component={MainPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="ui-position--relative">
+			{window.location.pathname !== "/v2" ? (
+				<>
+					<Router>
+						<MenuContext.Provider value={{ showMenu, setShowMenu, showIcons }}>
+							<SideMenu />
+							<MainMenu />
+						</MenuContext.Provider>
+						<Background />
+						<Logo />
+						<Switch>
+							<Route path="/" exact component={MainPage} />
+							<Route path="/about" exact component={AboutPage} />
+							<Route path="/projects" exact component={ProjectsPage} />
+							<Route path="/experience" exact component={ExperiencePage} />
+							<Route path="/v2/" exact component={V2} />
+							<Route component={MainPage} />
+						</Switch>
+					</Router>
+				</>
+			) : (
+				<V2 />
+			)}
+		</div>
+	);
 };
 
 export default App;
---
+++ src/v2/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/index.scss";
+import classnames from "classnames";
+
+export const App = () => {
+  return <div className={classnames("ui-bg-black")}>
+    
+  </div>;
+};
