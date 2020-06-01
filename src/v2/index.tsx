35bec5850a30d9ca0118ad38df0a9b10d143cd1c
swornimbarahi <swornimbarahi@gmail.com>
2020-06-01 16:47:12 -0400

    Added icons for the mainpage

--- src/v2/index.tsx
+++ src/v2/index.tsx
@@ -8,18 +8,24 @@ import ModeContext from "./stores/ModeContext";
 import Test from "./components/Test";
 
 export const App = () => {
-  const [theme, setTheme] = useState(true);
+	const [theme, setTheme] = useState(true);
 
-  useEffect(() => {
-    if (theme) setTheme(true);
-  }, [theme]);
+	useEffect(() => {
+		if (theme) setTheme(true);
+	}, [theme]);
 
-  return (
-    <ModeContext.Provider value={{ theme }}>
-      <div className={classnames("ui-bg--black")}>
-        asdfasdf
-        <Test />
-      </div>
-    </ModeContext.Provider>
-  );
+	return (
+		<ModeContext.Provider value={{ theme }}>
+			<div
+				className={classnames(
+					"ui-bg--black",
+					"ui-display--min-height",
+          "ui-display--min-width",
+          
+				)}
+			>
+				<Test />
+			</div>
+		</ModeContext.Provider>
+	);
 };
---
+++ src/v2/utils/index.tsx
@@ -0,0 +1,9 @@
+import Theme from "./Themes";
+
+const Utils = () => {
+  return "utils";
+};
+
+export {Theme};
+export default Utils;
+
