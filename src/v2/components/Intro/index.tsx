fa252d57990310e92613791036f08ec256ac8f3b
swornimbarahi <swornimbarahi@gmail.com>
2020-06-01 18:00:03 -0400

    Added no drag and no select css properties in the design system

--- src/v2/components/Intro/index.tsx
+++ src/v2/components/Intro/index.tsx
@@ -8,9 +8,18 @@ import SwornimBarahi from "../../assets/pictures/swornimbarahi.png";
 const Intro = () => {
 	return (
 		<div
-			className={classnames("ui-text--center", "ui-bg--black", "ui-display--min-height", "ui-display--min-width")}
+			className={classnames(
+				"ui-text--center",
+				"ui-bg--black",
+				"ui-display--min-height",
+				"ui-display--min-width"
+			)}
 		>
-			<img src={SwornimBarahi} alt="" className={classnames("ui-p--20")}/>
+			<img
+				src={SwornimBarahi}
+				alt=""
+				className={classnames("ui-rel-p--20", "ui-no-select", "ui-no-drag")}
+			/>
 		</div>
 	);
 };
